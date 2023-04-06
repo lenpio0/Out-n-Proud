b1444c8f3e45b9d2bc1d97e328bd71c4392b95cc
lenpio0 <127386860+lenpio0@users.noreply.github.com>
2023-04-06 13:54:56 +0200

    Config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        
+        'blue': '#244CEC',
+        'pink': '#F41159',
+        'red': '#F81310',
+        'orange': '#FB7306',
+        'yellow': '#F7C809',
+        'green': '#077931',
+        'white': '#343030',
+        'black': '#FCFFFD',
       },
       fontFamily: {
 
