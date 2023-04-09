47a501360792401907302576cf33a070b2eba5ec
yoann <yoannmayoussier@gmail.com>
2023-04-09 21:53:03 +0200

    Suppression du pluglin "text shadow" dans tailwind + Ajout d'un petit script pour btn back2top (apparition/disparition)
    le pluglin pour incorporer le text shadow dans tailwind faisait buguer la configuration précédente. Celle du custom des couleurs et l'ajout des polices, d'où sa suppression.

---
+++ scripts/back2top.js
@@ -0,0 +1,14 @@
+let toTopButton = document.getElementById("to-top-button");
+
+    // Check if the button exists
+    if (toTopButton) {
+
+        // On scroll event, toggle button visibility based on scroll position
+        window.onscroll = function() {
+            if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+                toTopButton.classList.remove("hidden");
+            } else {
+                toTopButton.classList.add("hidden");
+            }
+        };
+    };
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,29 +22,3 @@ module.exports = {
   },
   plugins: [],
 }
-
-const plugin = require('tailwindcss/plugin')
-
-module.exports = {
-  theme: {
-    extend: {
-      textShadow: {
-        sm: '0 1px 2px var(--tw-shadow-color)',
-        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-        lg: '0 8px 16px var(--tw-shadow-color)',
-      },
-    },
-  },
-  plugins: [
-    plugin(function ({ matchUtilities, theme }) {
-      matchUtilities(
-        {
-          'text-shadow': (value) => ({
-            textShadow: value,
-          }),
-        },
-        { values: theme('textShadow') }
-      )
-    }),
-  ],
-}
