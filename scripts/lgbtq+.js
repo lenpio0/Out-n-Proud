74e0d7ebf0523178e85c2122c4ea2d7d3ab66ed7
lenpio0 <127386860+lenpio0@users.noreply.github.com>
2023-04-14 15:45:01 +0200

    accueil animations fonctionnelles

--- scripts/lgbtq+.js
+++ scripts/lgbtq+.js
@@ -1,32 +1,49 @@
 let rainbowCards = document.querySelector('#rainbow-cards');
 
-console.log(rainbowCards.children[1].children[0]);
-for (var i = 0; i < rainbowCards.children.length; i++) {
-    //console.log(rainbowCards.children[i].children[0].id);
-    const currentId = rainbowCards.children[i].children[0].id;
-    const currentElement = document.querySelector('#' + currentId);
-    const currentWord = currentElement.children[1];
-    console.log(currentWord);
-    //let lesbianWord = document.querySelector("#lesbian-word");
-    currentElement.addEventListener("mouseenter", () => {
-        //lesbianWord.classList.remove('hidden');
-        currentWord.classList.remove('hidden');
-        console.log('enter');
-    });
-    currentElement.addEventListener("mouseleave", () => {
-        //lesbianWord.classList.add('hidden');
-        currentWord.classList.add('hidden');
-        console.log('leave');
-    });
-}
+for (let i = 0; i < rainbowCards.children.length; i++) {
 
-// rainbowCards.children.foreach(element => {
-//     let card = element.children[0];
-//     let cardChild = card.children[1];
-//     card.addEventListener("mouseenter", () => {
-//         cardChild.classList.remove('hidden');
-//     })
-//     card.addEventListener("mouseleave", () => {
-//         cardChild.classList.add('hidden');
-//     })
-// });
+    // console.log(rainbowCards.children[i]);
+    // console.log(rainbowCards.children[i].children[0]);
+
+    let currentElement = rainbowCards.children[i];
+    let currentTextBox = currentElement.children[0];
+    let currentLetter = currentTextBox.children[0];
+    let currentWord = currentTextBox.children[1];
+    let currentDef = currentTextBox.children[2];
+
+    currentElement.addEventListener("mouseenter", () => {
+        if (currentElement.classList.contains('card-active') == false) {
+            currentWord.classList.replace('hidden', 'block');
+        }
+    });
+
+    currentElement.addEventListener("mouseleave", () => {
+        if (currentElement.classList.contains('card-active') == false) {
+            currentWord.classList.replace('block', 'hidden');
+        }
+    });
+    currentElement.addEventListener('click', () => {
+        if (currentElement.classList.contains('card-active')) {
+            for (let i = 0; i < rainbowCards.children.length; i++) {
+                rainbowCards.children[i].classList.remove('card-active');
+                rainbowCards.children[i].children[0].classList.remove('textbox-active'); 
+                rainbowCards.children[i].children[0].children[0].classList.remove('hidden');
+                rainbowCards.children[i].children[0].children[2].classList.add('hidden');
+            }
+        }
+        else {
+            for (let i = 0; i < rainbowCards.children.length; i++) {
+                rainbowCards.children[i].classList.remove('card-active');
+                rainbowCards.children[i].children[0].classList.remove('textbox-active'); 
+                rainbowCards.children[i].children[0].children[0].classList.remove('hidden');
+                rainbowCards.children[i].children[0].children[2].classList.add('hidden');
+  
+            }
+            currentElement.classList.add('card-active');
+            currentTextBox.classList.add('textbox-active');
+            currentLetter.classList.add('hidden');
+            currentWord.classList.add('hidden');
+            currentDef.classList.remove('hidden');
+        }
+    })
+}
