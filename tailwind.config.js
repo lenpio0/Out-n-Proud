3efd3c7d625b0a2630d57787eeeff54b0aa566cf
komiJESUS <127386761+komiJESUS@users.noreply.github.com>
2023-04-18 10:43:45 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,7 @@ module.exports = {
         'separator-top': 'linear-gradient(to bottom right, #FCFFFD 50%, #0000 50%)',
         'separator-bot': 'linear-gradient(to bottom right, #0000 50%, #FCFFFD 50%)',
         'smoke': 'url(../images/rainbowSmoke2.png)',
+        'smokel': 'url(/Assets/images/smokeLeft.png)',
       },
       maxWidth: {
         '15rem': '15rem',
